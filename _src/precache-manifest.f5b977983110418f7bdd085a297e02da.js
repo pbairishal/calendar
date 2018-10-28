c3f553cbfba8fad3478fd3fef156f37c86ea6242
Pradosh <pbairishal@gmail.com>
2018-10-29 00:04:19 +0530

    Setting up Firebase

---
+++ _src/precache-manifest.f5b977983110418f7bdd085a297e02da.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1f6b4264351995ca3470",
+    "url": "/static/js/main.1f6b4264.chunk.js"
+  },
+  {
+    "revision": "96dfa215dc6eaaac08c0",
+    "url": "/static/js/1.96dfa215.chunk.js"
+  },
+  {
+    "revision": "1f6b4264351995ca3470",
+    "url": "/static/css/main.c85692a2.chunk.css"
+  },
+  {
+    "revision": "2a9e7d4f061b2589c82b007ae9b490da",
+    "url": "/index.html"
+  }
+];
